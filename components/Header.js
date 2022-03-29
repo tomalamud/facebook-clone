b7f0e142113196e3218c4acd338a77f97db1fc12
Tomás Elizondo <tomaselizondo@piper.fibertel.com.ar>
2022-03-29 17:10:39 -0300

    responsive header

--- components/Header.js
+++ components/Header.js
@@ -6,13 +6,17 @@ import {
   PlayIcon,
   ShoppingCartIcon,
   UserGroupIcon,
+  ViewGridIcon,
+  ChatIcon,
+  ChevronDownIcon,
+  BellIcon,
  
 } from "@heroicons/react/solid"
 import HeaderIcon from "./HeaderIcon"
 
 function Header() {
   return (
-    <div className="flex p-2 sticky top-0 z-50 bg-white items-center lg:px-5 shadow-md">
+    <div className="flex p-3 sticky top-0 z-50 bg-white items-center md:px-5 shadow-md">
       {/* Left */}
        <div className="flex">
          <Image 
@@ -23,13 +27,13 @@ function Header() {
          />
          <div className="flex bg-gray-200 p-2 rounded-full ml-4 items-center">
           <SearchIcon className="w-6 text-gray-600"/>
-          <input type="text" className="bg-transparent placeholder-gray-400 outline-none" placeholder="Search Facebook"/>
+          <input type="text" className="hidden md:inline-flex bg-transparent placeholder-gray-400 outline-none" placeholder="Search Facebook"/>
          </div>
        </div>
       {/* Center */}
       <div className="flex justify-center flex-grow">
-        <div className="flex space-x-6 md:space-x-20">
-          <HeaderIcon Icon={HomeIcon}/>
+        <div className="flex space-x-1 md:space-x-10">
+          <HeaderIcon active Icon={HomeIcon}/>
           <HeaderIcon Icon={FlagIcon}/>
           <HeaderIcon Icon={PlayIcon}/>
           <HeaderIcon Icon={ShoppingCartIcon}/>
@@ -37,6 +41,15 @@ function Header() {
         </div>
       </div>
       {/* Right */}
+      <div className="flex items-center sm:space-x-2 justify-end">
+        {/* Image Profile */}
+
+        <p className="whitespace-nowrap font-semibold xl:pr-3">Tomás Elizondo</p>
+        <ViewGridIcon className="icon" />
+        <ChatIcon className="icon" />
+        <BellIcon className="icon" />
+        <ChevronDownIcon className="icon" />
+      </div>
     </div>
   )
 }
--- components/HeaderIcon.js
+++ components/HeaderIcon.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function HeaderIcon({ Icon }) {
+function HeaderIcon({ Icon, active }) {
   return (
-    <div className='flex items-center rounded-xl cursor-pointer md:px-10 hover:bg-gray-100 active:border-b-2 active:border-blue-300 group'>
-      <Icon  className="h-5 text-gray-500 my-2 px-2 group-hover:text-blue-500 text-center mx-auto"/>
+    <div className='flex items-center rounded-xl cursor-pointer md:hover:bg-gray-100 md:active:border-b-2 md:active:border-blue-300 group'>
+      <Icon  className={`h-5 text-gray-500 my-2 px-2 group-hover:text-blue-500 text-center mx-auto ${active && "text-blue-500"}`}/>
     </div>
   )
 }
